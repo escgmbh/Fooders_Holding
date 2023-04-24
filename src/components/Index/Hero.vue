ddf7573d6daa4a7f41d25f6671dc1f117fad4e0b
amrizjantan <manager@edgedept.com>
2023-04-24 11:53:38 +0200

    responsive and button hover changed to white
    Signed-off-by: amrizjantan <manager@edgedept.com>

--- src/components/Index/Hero.vue
+++ src/components/Index/Hero.vue
@@ -1,11 +1,16 @@
 <script></script>
 <template>
-    <div class="hero min-h-screen" style="">
-        <img src="/src/assets/images/FoodersLanding.png" alt="" />
-
-        <div class="hero-overlay bg-opacity-0 bg:gray-200"></div>
-        <div class="hero-content text-center text-neutral-content">
-            <div class="max-w-md mx-auto text-white">
+    <div class="hero min-h-screen relative">
+        <img
+            src="/src/assets/images/FoodersLanding.png"
+            alt=""
+            class="w-full h-full object-cover"
+        />
+        <div class="hero-overlay bg-opacity-5 absolute inset-0"></div>
+        <div
+            class="hero-content text-center text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        >
+            <div class="max-w-md mx-auto">
                 <h3 class="mb-5 text-3xl font-bold uppercase text-green-400">
                     fooders
                 </h3>
@@ -13,16 +18,20 @@
                     TASTE YOUR LIFE
                 </h1>
                 <p class="mb-5">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
                     modi temporibus ab quibusdam dolorem totam deleniti rem
                     facilis odio esse. Ratione veniam corrupti et est iste
                     inventore sit quam! Dolorum?
                 </p>
-                <div class="flex justify-center gap-10">
-                    <a class="btn btn-primary bg-orange-400">hungry</a>
-                    <button class="btn btn-primary bg-orange-400">
-                        starving
-                    </button>
+                <div class="flex flex-row justify-center gap-4 md:gap-10">
+                    <a
+                        class="btn btn-primary bg-orange-400 border-orange-400 hover:bg-white hover:text-orange-400 mb-4 md:mb-0"
+                        >hungry</a
+                    >
+                    <a
+                        class="btn btn-primary bg-orange-400 border-orange-400 hover:bg-white hover:text-orange-400 mb-4 md:mb-0"
+                        >hungry</a
+                    >
                 </div>
             </div>
         </div>
