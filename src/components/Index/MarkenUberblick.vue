ad150b1f6e824a8bf2e83ecc214345a7fd6e04c1
amrizjantan <manager@edgedept.com>
2023-04-24 14:55:44 +0200

    MarkenUberblick component created
    Signed-off-by: amrizjantan <manager@edgedept.com>

---
+++ src/components/Index/MarkenUberblick.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+<template>
+    <div
+        class="bg-foordersuberblickBG w-full h-48 items-center flex flex-row justify-evenly"
+    >
+        <div class="w-28 h-28 rounded-full bg-gray-300"></div>
+        <div class="w-28 h-28 rounded-full bg-gray-300"></div>
+        <div class="w-28 h-28 rounded-full bg-gray-300"></div>
+        <div class="w-28 h-28 rounded-full bg-gray-300"></div>
+        <div class="w-28 h-28 rounded-full bg-gray-300"></div>
+    </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Hero from '@/components/Index/Hero.vue';
+
+import MarkenUberblickt from '@/components/Index/MarkenUberblick.vue';
 </script>
 
-<template><Hero /></template>
+<template><Hero /><MarkenUberblickt /></template>
 
 <style></style>
