037532a72934ad8de60da6e366d21adc7085f0b7
amrizjantan <manager@edgedept.com>
2023-04-24 17:38:10 +0200

    footer cretaed
    Signed-off-by: amrizjantan <manager@edgedept.com>

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
 import Header from '@/components/Design/Default/Header.vue';
+import Footer from '@/components/Design/Default/Footer.vue';
 </script>
 
 <template>
     <div class="">
-
-        <div class="flex flex-col mx-auto max-w-7xl min-h-screen items-center justify-start bg-baseoffwhite shadow-xl ">
-
+        <div
+            class="flex flex-col mx-auto max-w-7xl min-h-screen items-center justify-start bg-baseoffwhite shadow-xl"
+        >
             <Header />
             <router-view></router-view>
+            <Footer />
         </div>
     </div>
 </template>
