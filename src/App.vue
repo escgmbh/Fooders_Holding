a2ad1ad66b627e0ab6e5f5f3690fbce417c5a764
amrizjantan <manager@edgedept.com>
2023-04-24 10:37:30 +0200

    Header component created and implemented
    Signed-off-by: amrizjantan <manager@edgedept.com>

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <script setup>
-import { useDark, useToggle } from '@vueuse/core';
-import { SunIcon, MoonIcon } from '@heroicons/vue/24/solid';
-
-const isDark = useDark({
-  selector: 'html',
-  attribute: 'data-theme',
-  valueDark: 'dark',
-  valueLight: 'light',
-});
-const toggleDark = useToggle(isDark);
+import Header from '@/components/Design/Default/Header.vue';
 </script>
 
 <template>
     <div class="w-screen h-screen">
-        <div class="ml-4 md:ml-6 flex justify-items-end">
-            <span class="cursor-pointer mx-2 xl:mx-0" @click="toggleDark()">
-                <component :is="isDark ? MoonIcon : SunIcon" class="h-6 w-6" />
-            </span>
-        </div>
+        <div class="ml-4 md:ml-6 flex justify-items-end"></div>
+        <Header />
         <router-view></router-view>
     </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,23 +1,5 @@
-<script setup>
-import { useI18n } from 'vue-i18n';
-import LanguageChooser from '../components/LanguageChooser.vue';
+<script setup></script>
 
-const { t } = useI18n();
-
-const getDynamicTranslation = () => `${t('greeting.subtitle')}!`;
-</script>
-
-<template>
-    <div
-        class="w-screen h-screen flex items-center justify-center bg-cover bg-base-100 flex-col"
-    >
-        <h1 class="text-base-200 text-5xl font-lato font-thin">
-            {{ $t('greeting.title') }}
-        </h1>
-        <br />
-        <h3 class="text-base-200 font-mono">{{ getDynamicTranslation() }}</h3>
-        <LanguageChooser></LanguageChooser>
-    </div>
-</template>
+<template>TEST</template>
 
 <style></style>
