968775a2bb498b9f8dece8a65edecb00086542e5
amrizjantan <manager@edgedept.com>
2023-04-25 11:38:28 +0200

    hero is now responsive
    Signed-off-by: amrizjantan <manager@edgedept.com>

--- src/components/Design/Default/Header.vue
+++ src/components/Design/Default/Header.vue
@@ -1,24 +1,29 @@
 <script setup></script>
 <template>
-    <div class=" w-full px-64">
-        <div class=" items-center bg-baseoffwhite">
-            <div
-                class="flex flex-row justify-between w-full  mx-auto"
-            >
-                <div class="w-1/4 flex flex-row justify-between self-center">
-                    <placeholder class="text-foodersorange">coming </placeholder>
+    <div class="w-full md:px-64">
+        <div class="items-center bg-baseoffwhite">
+            <div class="flex flex-row justify-between w-full mx-auto">
+                <div
+                    class="w-1/4 flex flex-row justify-between self-center hidden md:flex"
+                >
+                    <placeholder class="text-foodersorange">coming</placeholder>
                     <placeholder class="text-foodersorange">soon</placeholder>
                 </div>
-                <div class="w-1/2 flex justify-center mt-4">
+                <div class="w-1/2 flex justify-center mt-4 text-center">
                     <img
                         src="@/assets/images/FoodersLogo.png"
-                        alt=""
-                        class="max-w-full "
+                        alt="logo of Fooders"
+                        class="md:max-w-full mx-auto"
                     />
                 </div>
-                <div class="w-1/4 flex flex-row justify-between self-center">
+
+                <div
+                    class="w-1/4 flex flex-row justify-between self-center hidden md:flex"
+                >
                     <placeholder class="text-foodersorange">stay</placeholder>
-                    <placeholder class="text-foodersorange">excited</placeholder>
+                    <placeholder class="text-foodersorange"
+                        >excited</placeholder
+                    >
                 </div>
             </div>
         </div>
--- src/components/Index/Hero.vue
+++ src/components/Index/Hero.vue
@@ -1,17 +1,31 @@
 <script></script>
 <template>
-
-   <div class="bg-[url('/images/FoodersLanding.png')] h-heroimg w-full bg-no-repeat bg-contain flex justify-center relative">
-        <div class="bg-baseoffwhite/90 flex flex-col w-1/3 mb-2 p-14 absolute bottom-6">
+    <div
+        class="bg-[url('/images/FoodersLanding.png')] h-heroimg w-full bg-no-repeat bg-contain flex justify-center relative"
+    >
+        <div
+            class="bg-baseoffwhite/90 md:flex md:flex-col md:w-1/3 mb-2 p-14 absolute bottom-6"
+        >
             <h2 class="text-foodersgreen text-3xl">FOODERS</h2>
             <h1 class="text-foodersdark text-4xl uppercase">taste your life</h1>
             <p class="text-foodersdark">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos earum, ipsa assumenda neque quis autem amet dignissimos magni quia recusandae, cupiditate magnam error voluptatibus. A harum eaque iste quia placeat.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
+                earum, ipsa assumenda neque quis autem amet dignissimos magni
+                quia recusandae, cupiditate magnam error voluptatibus. A harum
+                eaque iste quia placeat.
             </p>
             <div class="flex flex-row mx-2 justify-around mt-2">
-                <button class="bg-foodersorange text-white font-bold p-3 hover:bg-transparent hover:border-2 hover:border-foodersorange hover:text-foodersorange">click me</button>
-                <button class="border-foodersorange border-2 text-foodersorange font-bold p-3 hover:bg-foodersorange hover:text-white">click me</button>
+                <button
+                    class="bg-foodersorange text-white font-bold p-3 hover:bg-transparent hover:border-2 hover:border-foodersorange hover:text-foodersorange"
+                >
+                    click me
+                </button>
+                <button
+                    class="border-foodersorange border-2 text-foodersorange font-bold p-3 hover:bg-foodersorange hover:text-white"
+                >
+                    click me
+                </button>
             </div>
         </div>
-   </div>
+    </div>
 </template>
