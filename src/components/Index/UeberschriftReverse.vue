6b559be74aa801c510a5f0f12fe6ec097de1c02c
amrizjantan <manager@edgedept.com>
2023-04-25 10:27:58 +0200

    UeberschriftReverse is reponsive
    Signed-off-by: amrizjantan <manager@edgedept.com>

--- src/components/Index/UeberschriftReverse.vue
+++ src/components/Index/UeberschriftReverse.vue
@@ -1,13 +1,15 @@
 <script setup></script>
 <template>
     <div class="py-32">
-        <div class="md:flex md:flex-row">
-            <div class="w-6/12 flex justify-center items-center px-10">
+        <div class="flex flex-col-reverse md:flex md:flex-row">
+            <div class="md:w-6/12 flex justify-center items-center px-10 mt-4">
                 <div class="">
-                    <h1 class="text-foodersdark text-4xl font-bold text-right">
+                    <h1
+                        class="text-foodersdark text-4xl font-bold md:text-right"
+                    >
                         Überschrift h1
                     </h1>
-                    <p class="text-foodersdark mt-4 text-right">
+                    <p class="text-foodersdark md:mt-4 md:text-right">
                         Lorem, ipsum dolor sit amet consectetur adipisicing
                         elit. Eos est, fuga debitis minima asperiores in
                         deleniti, hic libero exercitationem rerum enim nihil
@@ -20,7 +22,7 @@
                     </p>
                 </div>
             </div>
-            <div class="w-auto md:w-6/12 ml-10">
+            <div class="w-auto md:w-6/12 md:ml-10">
                 <img src="@/assets/images/Uebersicht.png" alt="" />
             </div>
         </div>
