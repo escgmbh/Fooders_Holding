01f6b1d54dae325ff3dec3ec71f5268fa64042f6
amrizjantan <manager@edgedept.com>
2023-04-25 11:17:03 +0200

    markenuberblick responsive but needs adjustment
    Signed-off-by: amrizjantan <manager@edgedept.com>

--- src/components/Index/MarkenUberblick.vue
+++ src/components/Index/MarkenUberblick.vue
@@ -1,17 +1,14 @@
 <script setup></script>
 <template>
     <div
-        class="bg-foordersuberblickBG w-full h-48 flex flex-wrap justify-between items-center"
+        class="bg-foordersuberblickBG w-full h-60 md:h-48 flex flex-wrap justify-between items-center"
     >
-        <div class="w-1/2 sm:w-28 h-28 rounded-full bg-gray-300 mb-4"></div>
-        <div class="w-1/2 sm:w-28 h-28 rounded-full bg-gray-300 mb-4"></div>
         <div
             class="hidden sm:block w-28 h-28 rounded-full bg-gray-300 mb-4"
         ></div>
-        <div class="w-1/2 sm:w-28 h-28 rounded-full bg-gray-300 mb-4"></div>
-        <div class="w-1/2 sm:w-28 h-28 rounded-full bg-gray-300 mb-4"></div>
-        <div
-            class="hidden sm:block w-28 h-28 rounded-full bg-gray-300 mb-4"
-        ></div>
+        <div class="w-28 h-28 rounded-full bg-gray-300 mb-4"></div>
+        <div class="w-28 h-28 rounded-full bg-gray-300 mb-4"></div>
+        <div class="w-28 h-28 rounded-full bg-gray-300 mb-4"></div>
+        <div class="w-28 h-28 rounded-full bg-gray-300 mb-4"></div>
     </div>
 </template>
