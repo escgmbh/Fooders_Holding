e0e722c813a7690fd7127e8cfd98a81173e723a5
amrizjantan <manager@edgedept.com>
2023-04-26 11:42:27 +0200

    images added accordingly added to markenuberblick component as test images
    Signed-off-by: amrizjantan <manager@edgedept.com>

--- src/components/Index/MarkenUberblick.vue
+++ src/components/Index/MarkenUberblick.vue
@@ -3,12 +3,40 @@
     <div
         class="bg-foordersuberblickBG mx-4 w-full h-60 md:h-52 grid grid-cols-2 md:grid-cols-5 gap-3 content-center my-4 place-items-center space-y-1"
     >
-        <div
-            class="hidden sm:block w-24 h-24 rounded-full bg-gray-300 mb-4"
-        ></div>
-        <div class="w-24 h-24 rounded-full bg-gray-300 mb-4"></div>
-        <div class="w-24 h-24 rounded-full bg-gray-300 mb-4"></div>
-        <div class="w-24 h-24 rounded-full bg-gray-300 mb-4"></div>
-        <div class="w-24 h-24 rounded-full bg-gray-300 mb-4"></div>
+        <div class="hidden sm:block w-28 h-28 rounded-full bg-gray-700 mb-4">
+            <img
+                src="/public/images/asiahublogo.png"
+                alt=""
+                class="w-28 h-28 rounded-full object-contain"
+            />
+        </div>
+        <div class="w-28 h-28 rounded-full bg-gray-300 mb-4">
+            <img
+                src="/public/images/mrclou_logo-1.png"
+                alt=""
+                class="w-28 h-28 rounded-full object-contain"
+            />
+        </div>
+        <div class="w-28 h-28 rounded-full bg-gray-700 mb-4">
+            <img
+                src="/public/images/asiahublogo.png"
+                alt=""
+                class="w-28 h-28 rounded-full object-contain"
+            />
+        </div>
+        <div class="w-28 h-28 rounded-full bg-gray-300 mb-4">
+            <img
+                src="/public/images/mrclou_logo-1.png"
+                alt=""
+                class="w-28 h-28 rounded-full object-contain"
+            />
+        </div>
+        <div class="w-28 h-28 rounded-full bg-gray-700 mb-4">
+            <img
+                src="/public/images/asiahublogo.png"
+                alt=""
+                class="w-28 h-28 rounded-full object-contain"
+            />
+        </div>
     </div>
 </template>
